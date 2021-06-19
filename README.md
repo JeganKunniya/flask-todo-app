4dfbb005d978a1f2082bc4167a2ac412ac050fcb
Jegan Kunniya <jegan.kunniya@gmail.com>
2021-06-20 02:12:09 +0530

    Basic functionality of a flask app. The todo app will be built on top of this foundation.

---
+++ README.md
@@ -0,0 +1,8 @@
+Hello there,
+
+Welcome to my todo application built using Python and Flask.
+
+This app has been built as part of the learning curriculum of 'Python to Projects' 
+offered and taught virtually by **@bhavaniravi_** and **@TheLearningDev**.
+
+Wish me luck!
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # This is the main program to run the flask web application
 
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/<name>')
+def echo(name):
+    return f'Hello, {name}!'
+
+
 if __name__ == '__main__':
-    pass
+    app.run(debug=True)  # live mode enabled with debug=True
